56b75470257f2d6c853041f7f7ddb60cff4b4883
yhs233 <dazhiyouhuashuo@gmail.com>
2023-08-21 18:51:34 +0800

    Update nw.js

--- nw.js
+++ nw.js
@@ -4,28 +4,28 @@ const svc = new Service({
 	name: 'unblock-netease-cloud-music',
 	description: '点亮网易云音乐灰色歌曲',
 	script: './app.js', // 入口文件路径
-	// scriptOptions:'-p 2233:3344', // 可选参数示例: 自定义端口并开启HTTPS
-	// scriptOptions: '-o kugou ytdlp pyncmd', // 可选参数
+	scriptOptions:'-p 2233:3344', // 可选参数示例: 自定义端口并开启HTTPS
+	scriptOptions: '-o kugou ytdlp pyncmd', // 可选参数
 	wait: '1', // 程序崩溃后重启时间间隔
 	grow: '0.25', // 重启等待时间成长值，第一次1秒，第二次1.25秒。。。
 	maxRestarts: '40', // 60秒内最大重启次数
 	env: [
-		// {
-		// 	name: 'ENABLE_FLAC',
-		// 	value 'true',
-		// },
-		// {
-		// 	name: 'QQ_COOKIE',
-		// 	value: 'uin=<your_uin>; qm_keyst=<your_qm_keyst>',
-		// },
-		// {
-		// 	name: 'ENABLE_LOCAL_VIP=svip',
-		// 	value: 'true',
-		// },
-		// {
-		// 	name: 'DISABLE_UPGRADE_CHECK=true',
-		// 	value: 'true',
-		// },
+		 {
+		 	name: 'ENABLE_FLAC',
+		 	value 'true',
+		 },
+		 //{
+		 //	name: 'QQ_COOKIE',
+		 //	value: 'uin=<your_uin>; qm_keyst=<your_qm_keyst>',
+		 //},
+		 {
+		 	name: 'ENABLE_LOCAL_VIP=svip',
+		 	value: 'true',
+		 },
+		 {
+			name: 'DISABLE_UPGRADE_CHECK=true',
+		 	value: 'true',
+		 },
 
 
 	],
